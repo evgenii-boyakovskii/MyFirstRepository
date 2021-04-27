722bd167a7d1106f1995f0a2103ec45fc680b216
Evgenii Boyakovskii <boyakovskii.evgenii@gmail.com>
2021-04-27 10:52:02 +0300

    Убрал лишнее

--- Arifmetika.py
+++ Arifmetika.py
@@ -8,27 +8,3 @@ try:
 except ZeroDivisionError as e:
     print("На ноль делить нельзя")
 
-
-print('Type "Quit" for exit')
-while True:
-    try:
-        sign = input('Выберите одну из операций:"+","-","/","*": ')
-        if sign == "Quit":
-            break
-        if sign not in ("+", "-", "/", "*"):
-            print("Введите одну из предложенных операций")
-            continue
-        a = float(input("a = "))
-        b = float(input("b = "))
-        if sign == "+":
-            print(a + b)
-        elif sign == "-":
-            print(a - b)
-        elif sign == "*":
-            print(a * b)
-        elif sign == "/":
-            print(a / b)
-    except ZeroDivisionError as ex:
-        print("На ноль делить нельзя")
-    except ValueError as e:
-        print("Введите целое число: ")
